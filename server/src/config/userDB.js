ad0acb97fa9ec4a99aa0d679a924562b24055005
Saint. Clever <parchment@gmx.com>
2024-12-13 22:31:45 -0500

    Update mongoose

--- server/src/config/userDB.js
+++ server/src/config/userDB.js
@@ -4,10 +4,7 @@ const MONGODB_URI = process.env.MONGODB_URI;
 // Connect to MongoDB
 const userDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI, {
-      "useNewUrlParser": true,
-      "useUnifiedTopology": true,
-    });
+    await mongoose.connect(MONGODB_URI);
     console.log("Mongoose Connection Successful!");
   } catch (error) {
     console.log("Mongoose Connection Failed!");
